931b445c0c2f0cfbdb358626d30a9152df7b0ef2
Andrew Reppucci <andrewreppucci@Andrews-MacBook-Pro-2.local>
2020-04-13 12:32:27 -0400

    stuff

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -1,10 +1,122 @@
 import React, { Component } from 'react';
+import Rating from 'react-rating';
+import Music from './music.js'
 
 export class QuizComponent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isSure: true,
+      acousticness: 0,
+      danceability: 0,
+      energy: 0,
+      instrumentalness: 0,
+      loudness: 0,
+      valence: 0,
+      tempo: 0,
+
+      //what they like about hte song
+      song1id: null,
+      song1play: null,
+      pic1: null,
+      song2id: null,
+      song2play: null,
+      pic2: null,
+      song2id:null,
+      song2play: null,
+      pic3: null,
+      hasSongsToDisplay: false,
+      isloading:false
+
+    };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  // handleChange(event) {
+  //   this.setState({ name: event.target.value });
+  // }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+  //     .then(response => response.json())
+  //     .then(state => this.setState(state));
+  // }
+
+  callGenerateSong = () => {
+    if(this.state.isloading){
+      return
+    }
+    this.setState({isloading:true})
+    fetch('http://localhost:3001/getRandomSong')
+    .then((response) => {
+      return response.json();
+    })
+    .then((track) => {
+      let data = track.track
+      console.log(data);
+      this.setState({isloading:false,
+        song1id: data[0].id,
+        song1play: data[0].preview_url,
+        pic1: data[0].album.images[1].url,
+        song2id: data[1].id,
+        song2play: data[1].preview_url,
+        pic2: data[1].album.images[1].url,
+        song3id: data[2].id,
+        song3play: data[2].preview_url,
+        pic3: data[2].album.images[1].url,
+        hasSongsToDisplay: true,
+        isSure:false,
+
+    })
+  })
+}
+
+  sure = () => {return(<>
+  <h1> I know What I want (Only pick what you care about)</h1>
+
+  <h5> Acousticness Level </h5>
+  <Rating initialRating={this.state.acousticness} start={0} stop={10} onChange={value => {this.setState({acousticness:value})}}/>
+
+  <h5> Dancability  </h5>
+  <Rating initialRating={this.state.danceability} start={0} stop={10} onChange={value => {this.setState({danceability:value})}}/>
+
+  <h5> Energy Level </h5>
+  <Rating initialRating={this.state.energy} start={0} stop={10} onChange={value => {this.setState({energy:value})}}/>
+
+  <h5> Instrumentalness </h5>
+  <Rating initialRating={this.state.instrumentalness} start={0} stop={10} onChange={value => {this.setState({instrumentalness:value})}}/>
+
+  <h5> Happiness Level </h5>
+  <Rating initialRating={this.state.valence} start={0} stop={10} onChange={value => {this.setState({valence:value})}}/>
+
+  <h5> Tempo (Low to High)</h5>
+  <Rating initialRating={this.state.tempo} start={0} stop={10} onChange={value => {this.setState({tempo:value})}}/>
+
+  <h5> Loudness </h5>
+  <Rating initialRating={this.state.loudness} start={0} stop={10} onChange={value => {this.setState({loudness:value})}}/>
+
+  <h5> Finish Quiz </h5>{/* put call to server here and put state chance to song thing there */}
+  <button onClick={() => {this.callGenerateSong()}}> Help Me Finish</button>
+</>)}
+
+
+songpicker = () => {
+  return(
+    <>
+      <Music url={this.state.song1play} image={this.state.pic1}></Music>
+      <Music url={this.state.song2play} image={this.state.pic2}></Music>
+      <Music url={this.state.song3play} image={this.state.pic3}></Music>
+    </>
+  )
+}
+
   render() {
-    return <h3>Quiz!</h3>
-  }
+    return (
+      <>
+      {this.state.isSure && this.sure()}
+      {this.state.hasSongsToDisplay && this.songpicker()}
+      </>
+    )}
 }
---
+++ src/music.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+
+class Music extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            play: false
+        
+          };
+        
+        this.audio = new Audio(this.props.url)
+        
+        }
+  
+    componentDidMount() {
+      this.audio.addEventListener('ended', () => this.setState({ play: false }));
+    }
+  
+    componentWillUnmount() {
+      this.audio.removeEventListener('ended', () => this.setState({ play: false }));  
+    }
+  
+    togglePlay = () => {
+        console.log(this.props.url)
+      this.setState({ play: !this.state.play }, () => {
+        this.state.play ? this.audio.play() : this.audio.pause();
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <img src={this.props.image} />
+          <button onClick={this.togglePlay}>{this.state.play ? 'Pause' : 'Play'}</button>
+        </div>
+      );
+    }
+  }
+  
+  export default Music;
